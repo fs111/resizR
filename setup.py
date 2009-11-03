5f1ab476b57a623efa490c29ab5fd2a7c0ac8313
Andre Kelpe <efeshundertelf@googlemail.com>
2009-11-03 23:00:04 +0100

    added setup.py using distutils

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup 
+
+setup (name = 'resizR', 
+       description = "resizR is a pyinotify PIL hack to resize images automatically", 
+       author = "André Kelpe", 
+       author_email = "efeshundertelf@googlemail.com", 
+       version = '0.2', 
+       packages = ['resizRmodules',],
+       scripts = ["src/resizR"],
+       package_dir = {'': 'src'}
+       )
+
