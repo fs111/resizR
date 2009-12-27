7642a3172f74c92d2b22b9454d6bdb2fddefcc70
André Kelpe <fs111@theia.olymp>
2009-12-27 12:59:46 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ from distutils.core import setup
 setup (name = 'resizR', 
        description = "resizR is a pyinotify PIL hack to resize images automatically", 
        author = "André Kelpe", 
-       author_email = "efeshundertelf@googlemail.com", 
+       author_email = "efeshundertelf [at] googlemail [dot] com", 
        version = '0.2', 
        packages = ['resizRmodules',],
        scripts = ["src/resizR"],
        url = "http://github.com/fs111/resizR",
        package_dir = {'': 'src'}
        )
-
